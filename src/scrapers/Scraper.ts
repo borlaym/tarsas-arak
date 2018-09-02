0a3e6bbb7caa732577ee8adf837e6a9dc1f9cea6
Marton Borlay <borlaym@gmail.com>
2018-09-02 13:08:33 +0200

    refactor parseitem in szellemlovas

--- src/scrapers/Scraper.ts
+++ src/scrapers/Scraper.ts
@@ -1,4 +1,4 @@
-import Item, { Vendor } from "../Item";
+import Item, { Vendor, Language } from "../Item";
 import { compact } from "lodash";
 
 const SERVER_URL = window.location.hostname === 'localhost' ? 'http://localhost:3001/' : 'https://tarsas-kereso.herokuapp.com/';
@@ -13,16 +13,22 @@ abstract class Scraper {
 	 */
 	protected abstract readonly itemSelector: string;
 	/**
-	 * Parse a single item
-	 */
-	protected abstract parseItem(el: HTMLElement): Item | null;
-	/**
 	 * Whether the whole page should be parsed or not.
 	 * For example, gemklub will show you unrelated items if there are no search results
 	 */
 	protected shouldParsePage(el: Document): boolean {
 		return true;
 	}
+	/**
+	 * Various field getters
+	 */
+	protected abstract getTitle(el: Element): string;
+	protected abstract getLanguage(el: Element): Language;
+	protected abstract getPrice(el: Element): { original: number, discounted: number };
+	protected abstract getAvailable(el: Element): boolean;
+	protected abstract getNextAvailable(el: Element): string | null;
+	protected abstract getImageSrc(el: Element): string;
+	protected abstract getUrl(el: Element): string;
 	/**
 	 * Do a search for items of a query
 	 */
@@ -44,6 +50,41 @@ abstract class Scraper {
 			console.log(err);
 			return [];
 		}
+	}
+	/**
+	 * Parse a single item
+	 */
+	private parseItem(el: HTMLElement): Item {
+		return {
+			vendor: this.vendor,
+			title: this.getTitle(el),
+			language: this.getLanguage(el),
+			price: this.getPrice(el),
+			available: this.getAvailable(el),
+			nextAvailable: this.getNextAvailable(el),
+			image: this.getImageSrc(el),
+			url: this.getUrl(el)
+		}
+	}
+	/**
+	 * Helper for getting an element with standardized error handling
+	 */
+	protected getChild<T extends HTMLElement>(parent: Element, selector: string): T {
+		const result: T | null = parent.querySelector(selector);
+		if (!result) {
+			throw new Error('Unable to find element ' + selector + ' in scraper ' + this.vendor)
+		}
+		return result
+	}
+	/**
+	 * Helper for getting the text content of an element with standardized logging
+	 */
+	protected getTextContent(el: Element): string {
+		const textContent = el.textContent
+		if (!textContent) {
+			console.log('No text content in element ' + el.className + ' in scraper ' + this.vendor)
+		}
+		return textContent || ''
 	}
 }
 
--- src/scrapers/szellemlovas.ts
+++ src/scrapers/szellemlovas.ts
@@ -1,27 +1,6 @@
 import Item, { Language, Vendor } from "../Item";
 import Scraper from "./Scraper";
 
-function getAvailable(el: Element): boolean {
-	const text = (el.textContent || '');
-	if (text.indexOf('Nem rendelhető') > -1) {
-		return false;
-	}
-	if (text.indexOf('Azonnal kapható') > -1) {
-		return true;
-	}
-	if (text.indexOf('Várható érkezés') > -1) {
-		return false;
-	}
-	if (text.indexOf('Kikölcsönözve') > -1) {
-		return false;
-	}
-	if (text.indexOf('Előrendelhető') > -1) {
-		return false;
-	}
-	console.log('Not parsed availability on Gemklub: ' + text);
-	return true;
-}
-
 function getNextAvailable(el: Element): string | null {
 	const text = el.textContent || ''
 	if (text.indexOf('Azonnal kapható') > -1) {
@@ -33,10 +12,64 @@ function getNextAvailable(el: Element): string | null {
 class SzellemlovasScraper extends Scraper {
 	public vendor = Vendor.Szellemlovas;
 	protected itemSelector = '.items .view';
+	protected getTitle(el: Element): string {
+		return this.getTextContent(this.getChild(el, '.listcim'))
+	}
+	protected getLanguage(el: Element): Language {
+		const text = this.getTextContent(this.getChild(el, '#list_3h'))
+		// TODO nyelvfuggetlen
+		return text.indexOf('Magyar nyelvű') > -1 ? Language.Hungarian : Language.English;
+	}
+	protected getPrice(el: Element): { original: number; discounted: number; } {
+		const normalPriceEl = el.querySelector('.normalprice');
+		const originalPriceEl = el.querySelector('.originalprice');
+		const discountPriceEl = el.querySelector('.discountprice');
+		const orderable = this.getTextContent(this.getChild(el, '.szallitasi_ido')).indexOf('Nem rendelhető') === -1
+		const priceToUse = originalPriceEl || normalPriceEl;
+		return {
+			original: orderable && priceToUse ? parseInt(priceToUse.textContent || '', 10) : 0,
+			discounted: orderable && discountPriceEl ? parseInt(discountPriceEl.textContent || '', 10) : 0
+		}
+	}
+	protected getAvailable(el: Element): boolean {
+		const text = this.getTextContent(this.getChild(el, '.szallitasi_ido'))
+		if (text.indexOf('Nem rendelhető') > -1) {
+			return false;
+		}
+		if (text.indexOf('Azonnal kapható') > -1) {
+			return true;
+		}
+		if (text.indexOf('Várható érkezés') > -1) {
+			return false;
+		}
+		if (text.indexOf('Kikölcsönözve') > -1) {
+			return false;
+		}
+		if (text.indexOf('Előrendelhető') > -1) {
+			return false;
+		}
+		console.log('Not parsed availability on Gemklub: ' + text);
+		return true;
+	}
+	protected getNextAvailable(el: Element): string | null {
+		const text = this.getTextContent(this.getChild(el, '.szallitasi_ido'))
+		if (text.indexOf('Azonnal kapható') > -1) {
+			return null;
+		}
+		return text
+	}
+	protected getImageSrc(el: Element): string {
+		const image: HTMLImageElement = this.getChild(el, '#list_1h img')
+		return image.src.replace(window.location.origin, 'https://www.szellemlovas.hu')
+	}
+	protected getUrl(el: Element): string {
+		const link: HTMLAnchorElement = this.getChild(el, '.listcim a')
+		return link.href.replace(window.location.origin, 'https://www.szellemlovas.hu')
+	}
 	protected parseItem(el: HTMLElement): Item | null {
 		try {
 
-			const titleEl = el.querySelector('.listcim');
+			
 			const linkEl: HTMLAnchorElement | null = el.querySelector('.listcim a');
 			const normalPriceEl = el.querySelector('.normalprice');
 			const originalPriceEl = el.querySelector('.originalprice');
@@ -47,16 +80,16 @@ class SzellemlovasScraper extends Scraper {
 			if (titleEl && availability && imageEl && details && linkEl) {
 				const orderable = (availability.textContent || '').indexOf('Nem rendelhető') === -1;
 				const priceToUse = originalPriceEl || normalPriceEl;
-				const language = (details.textContent || '').indexOf('Magyar nyelvű') > -1 ? Language.Hungarian : Language.English;
+				const language = (details.textContent || '')
 				return {
-					title: titleEl.textContent || '',
+					title: ,
 					language,
 					price: {
 						original: orderable && priceToUse ? parseInt(priceToUse.textContent || '', 10) : 0,
 						discounted: orderable && discountPriceEl ? parseInt(discountPriceEl.textContent || '', 10) : 0
 					},
 					available: getAvailable(availability),
-					image: imageEl.src.replace(window.location.origin, 'https://www.szellemlovas.hu'),
+					image: 
 					vendor: Vendor.Szellemlovas,
 					nextAvailable: getNextAvailable(availability),
 					url: linkEl.href.replace(window.location.origin, 'https://www.szellemlovas.hu')
